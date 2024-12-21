ccffe6b966c53cd720a64d7bd0c9552fea6d4635
zenonet <78077158+zenonet@users.noreply.github.com>
2024-12-22 00:37:05 +0100

    Made web editor use workers

---
+++ Hlf.Web/wwwroot/worker.js
@@ -0,0 +1,7 @@
+﻿import bootsharp, {HlfTranspilerJs} from "./../bin/bootsharp/index.mjs";
+bootsharp.boot();
+
+onmessage = (e) => {
+    const res = HlfTranspilerJs.transpileToString(e.data);
+    postMessage(res);
+};
