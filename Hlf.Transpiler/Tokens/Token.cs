4cb43a13fda9b0754c0ffd0e487d195fbb958570
zenonet <78077158+zenonet@users.noreply.github.com>
2024-12-21 15:13:35 +0100

    Added Token.GetOperatorPrecedence()

--- Hlf.Transpiler/Tokens/Token.cs
+++ Hlf.Transpiler/Tokens/Token.cs
@@ -10,6 +10,23 @@ public class Token
     public override string ToString() => $"{Type}: '{Content}' (line {Line}, column {Column})";
 
     public static bool IsOperatorToken(TokenType type) => type is TokenType.DoubleEquals or TokenType.Asterisk or TokenType.Plus or TokenType.Minus or TokenType.Slash or TokenType.Percent;
+
+    public static int GetOperatorPrecedence(TokenType type)
+    {
+        return type switch
+        {
+            TokenType.DoubleEquals => 5,
+            
+            TokenType.Asterisk => 2,
+            TokenType.Slash => 2,
+            TokenType.Percent => 2,
+            
+            TokenType.Plus => 1,
+            TokenType.Minus => 1,
+            
+            _ => throw new ArgumentException($"Token type {type} is not an operator or its precedence hasn't been set"),
+        };
+    }
 }
 
 public enum TokenType
