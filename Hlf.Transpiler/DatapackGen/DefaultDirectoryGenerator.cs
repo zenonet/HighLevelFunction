ce74a0bc48d87000dc4c5d1b1b0a257bf6502a85
zenonet <78077158+zenonet@users.noreply.github.com>
2024-12-22 13:28:40 +0100

    Made DefaultDirectoryGenerator clear old files

--- Hlf.Transpiler/DatapackGen/DefaultDirectoryGenerator.cs
+++ Hlf.Transpiler/DatapackGen/DefaultDirectoryGenerator.cs
@@ -4,6 +4,8 @@ public class DefaultDirectoryGenerator : IDirectoryGenerator
 {
     public void GenerateDirectoryStructure(string parentPath, List<File> files)
     {
+        int slashIndex = files[0].Path.Replace('\\', '/').IndexOf('/');
+        Directory.Delete(Path.Join(parentPath, slashIndex == -1 ? files[0].Path : files[0].Path[..slashIndex]), true);
         foreach (File file in files)
         {
             string path = Path.Join(parentPath, file.Path);
