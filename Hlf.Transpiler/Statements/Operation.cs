69118da16ca677d45a54ad2a76f8182045799fdc
zenonet <78077158+zenonet@users.noreply.github.com>
2025-01-17 22:42:51 +0100

    Allowed any float-pair-operation to also be applied to an int and a float

--- Hlf.Transpiler/Statements/Operation.cs
+++ Hlf.Transpiler/Statements/Operation.cs
@@ -14,10 +14,25 @@ public class Operation : Statement
         A.Parse();
         B.Parse();
 
+        FindOperationDefinition();
+        
+        Result = definition!.ResultType.NewDataId();
+    }
+    
+    private void FindOperationDefinition()
+    {
+        if ((A.Result.Type == HlfType.Float && B.Result.Type == HlfType.Int) || (A.Result.Type == HlfType.Int && B.Result.Type == HlfType.Float))
+        {
+            // Allow float operations between any operands of type int and float
+            definition = HlfType.Float.Operations.FirstOrDefault(x => x.Operator == Op.Type && x.OtherType == HlfType.Float);
+            if (definition != null) return;
+            
+            // let the normal logic throw the normal error
+        }
+        
         definition = A.Result.Type.Operations.FirstOrDefault(x => x.OtherType == B.Result.Type && x.Operator == Op.Type);
         if (definition == null)
-            throw new LanguageException($"Type {A.Result.Type.Name} does not implement the {Op.Content} Operator with a right side of type {B.Result!.Type.Name}", Op);
-        Result = definition.ResultType.NewDataId();
+            throw new LanguageException($"Type {A.Result.Type.Name} does not implement the {Op.Content} Operator with a right side of type {B.Result.Type.Name}", Op);
     }
     
     public override string Generate(GeneratorOptions gen)
